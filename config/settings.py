a32fbf5cb4a8b433f5e3afd4b82195978a5c0a1b
nikhil12006-08 <nkumar37845@gmail.com>
2026-08-22 08:51:31 +0530

    Disable debug mode in production

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
